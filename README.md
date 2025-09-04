eddb1ed008333f4b4456a32b52d610023f41308d
Kresten Skovsted Buch <info@skovstedconsulting.dk>
2025-09-04 21:52:05 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hello World (Frappe App)
+
+A minimal Frappe/ERPNext app that provides:
+
+- API endpoint: `/api/method/hello_world.api.greeting`
+- Web page: `/hello`
--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="hello_world",
+    version="0.0.1",
+    description="Minimal Hello World app for ERPNext",
+    author="You",
+    author_email="you@example.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
